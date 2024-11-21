a41cf1df3f09084b3392bb3de82300c33a3e8b29
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2024-11-22 05:05:12 +0600

    added eslint and Prettier setup

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 import path from "path";
 
 // Load environment variables from .env file
-dotenv.config({path: path.join(process.cwd(), ".env")});
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    port: process.env.PORT,
-    db_url: process.env.MONGO_URL
-}
-
+  port: process.env.PORT,
+  db_url: process.env.MONGO_URL,
+};
